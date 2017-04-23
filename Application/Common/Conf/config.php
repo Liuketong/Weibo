25c348423a76e0900f41038b9efead14a1cfd178
Liuketong <617396279@qq.com>
2017-04-23 13:07:50 +0800

    微博项目初始版本

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    //设置可访问目录
+    'MODULE_ALLOW_LIST'=>array('Home','Admin'),
+    //设置默认目录
+    'DEFAULT_MODULE'=>'Home',
+    //设置默认主题目录
+    'DEFAULT_THEME'=>'Default'
+);
---
+++ Application/Runtime/Cache/Home/b917e53d0c499e83875a587119f99d48.php
@@ -0,0 +1,17 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>微博系统--登录首页</title>
+    <script type="text/javascript" src="/weibo/Public/Home/js/jquery.js"></script>
+    <script type="text/javascript" src="/weibo/Public/Home/js/login.js"></script>
+    <script type="text/javascript" src="/weibo/Public/Home/js/jquery.ui.js"></script>
+    <link rel="stylesheet" href="/weibo/Public/Home/css/jquery.ui.css"/>
+    <link rel="stylesheet" href="/weibo/Public/Home/css/login.css"/>
+</head>
+<body>
+
+
+
+</body>
+</html>
